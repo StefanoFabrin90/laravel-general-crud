b625fb94457bb340ecd3965f567006476f3b9de1
stef_fab@hotmail.it <stef_fab@hotmail.it>
2022-01-28 14:30:33 +0100

    add 1

---
+++ blog/resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="container">
+        <h1 class="text-center mb-5">Welcome in homepage</h1>
+
+        <p class="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam fugiat iusto, eos expedita qui cumque! Obcaecati velit quod nulla natus optio eius maxime voluptate? Nam nisi illum doloremque quisquam non?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam fugiat iusto, eos expedita qui cumque! Obcaecati velit quod nulla natus optio eius maxime voluptate? Nam nisi illum doloremque quisquam non?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam fugiat iusto, eos expedita qui cumque! Obcaecati velit quod nulla natus optio eius maxime voluptate? Nam nisi illum doloremque quisquam non?</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam fugiat iusto, eos expedita qui cumque! Obcaecati velit quod nulla natus optio eius maxime voluptate? Nam nisi illum doloremque quisquam non?</p>
+    </div>
+@endsection
---
+++ blog/resources/views/partials/footer.blade.php
@@ -0,0 +1,5 @@
+<footer>
+    <div class="container">
+        <p class="text-center mt-5">this is the footer</p>
+    </div>
+</footer>
